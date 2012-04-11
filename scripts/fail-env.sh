4bdf742056204c8d53ee9bd2b362c00b0d96fa77
hsc <hsc@8c4709b5-6ec9-48aa-a5cd-a96041d1645a>
2012-04-11 01:00:27 +0000

    hosts += bigbox
    git-svn-id: https://www4.informatik.uni-erlangen.de/i4svn/danceos/trunk/devel/fail@1050 8c4709b5-6ec9-48aa-a5cd-a96041d1645a

--- scripts/fail-env.sh
+++ scripts/fail-env.sh
@@ -9,12 +9,12 @@
 # A whitespace-separated list of hosts to rsync the experiment data to.  This
 # is not necessarily the same list as FAIL_EXPERIMENT_HOSTS (see below), as
 # many hosts may share their homes via NFS.
-export FAIL_DISTRIBUTE_HOSTS=${FAIL_DISTRIBUTE_HOSTS:='ios kos virtuos plutonium'}
+export FAIL_DISTRIBUTE_HOSTS=${FAIL_DISTRIBUTE_HOSTS:='ios kos virtuos plutonium bigbox.informatik.uni-erlangen.de'}
 
 # A whitespace-separated list of hosts to run experiments on.  If the host name
 # is followed by a ':' and a number, this specifies the number of clients to
 # run on that host (defaults to #CPUs+1).
-export FAIL_EXPERIMENT_HOSTS=${FAIL_EXPERIMENT_HOSTS:="plutonium uran virtuos ios:6 kos:6 bohrium polonium radon $(for hostnr in $(seq 100 254); do echo fiws$hostnr; done)"}
+export FAIL_EXPERIMENT_HOSTS=${FAIL_EXPERIMENT_HOSTS:="bigbox.informatik.uni-erlangen.de plutonium uran virtuos ios:6 kos:6 bohrium polonium radon $(for hostnr in $(seq 100 254); do echo fiws$hostnr; done)"}
 
 # A homedir-relative directory on the distribution hosts where all necessary
 # Fail* ingredients reside (see multiple-clients.sh).
