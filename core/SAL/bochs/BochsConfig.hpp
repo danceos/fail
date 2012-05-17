1e16f72fe2ccbce91fcb222280ebb1a524a32a5c
adrian <adrian@8c4709b5-6ec9-48aa-a5cd-a96041d1645a>
2012-05-17 14:24:36 +0000

    Updated Bochs and OVP config header (new type timer_t).
    git-svn-id: https://www4.informatik.uni-erlangen.de/i4svn/danceos/trunk/devel/fail@1243 8c4709b5-6ec9-48aa-a5cd-a96041d1645a

--- core/SAL/bochs/BochsConfig.hpp
+++ core/SAL/bochs/BochsConfig.hpp
@@ -13,10 +13,11 @@ namespace sal
 typedef bx_address guest_address_t; //!< the guest memory address type
 typedef Bit8u*     host_address_t;  //!< the host memory address type
 #if BX_SUPPORT_X86_64
-  typedef Bit64u register_data_t;   //!< register data type (64 bit)
+  typedef Bit64u   register_data_t; //!< register data type (64 bit)
 #else
-  typedef Bit32u register_data_t;   //!< register data type (32 bit)
+  typedef Bit32u   register_data_t; //!< register data type (32 bit)
 #endif
+typedef int        timer_t;         //!< type of timer IDs
 
 };
 
--- core/SAL/ovp/OVPConfig.hpp
+++ core/SAL/ovp/OVPConfig.hpp
@@ -1,16 +1,16 @@
 #ifndef __OVP_CONFIG_HPP__
   #define __OVP_CONFIG_HPP__
 
-
 // Type definitions and configuration settings for
 // the OVP simulator.
  
 namespace sal
 {
 
-typedef uint32_t guest_address_t; //!< the guest memory address type
-typedef uint8_t*     host_address_t;  //!< the host memory address type
-typedef uint32_t register_data_t;   //!< register data type (32 bit)
+typedef uint32_t guest_address_t;  //!< the guest memory address type
+typedef uint8_t* host_address_t;   //!< the host memory address type
+typedef uint32_t register_data_t;  //!< register data type (32 bit)
+typedef int      timer_t;          //!< type of timer IDs
 
 };
 
