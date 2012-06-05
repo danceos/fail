362ed5bb18a925b796a25848c32da81a2f36b3d1
hellwig <hellwig@8c4709b5-6ec9-48aa-a5cd-a96041d1645a>
2012-06-05 12:04:10 +0000

    tabs -> whitespaces (dump-trace.py)
    git-svn-id: https://www4.informatik.uni-erlangen.de/i4svn/danceos/trunk/devel/fail@1298 8c4709b5-6ec9-48aa-a5cd-a96041d1645a

--- core/plugins/tracing/dump-trace.py
+++ core/plugins/tracing/dump-trace.py
@@ -15,33 +15,33 @@ trace_event = trace_pb2.Trace_Event()
 
 
 try:
-	f = open(sys.argv[1], "rb")
+    f = open(sys.argv[1], "rb")
 except IOError:
     print sys.argv[1] + ": Could not open file."
     sys.exit(-1)
 
 while 1:
-	# Read trace length
-	try:
-		lengthNO = f.read(4)
-		if len(lengthNO) == 0:
-			break
-	except IOError:
-		print "Could not read data from file"
+    # Read trace length
+    try:
+        lengthNO = f.read(4)
+        if len(lengthNO) == 0:
+            break
+    except IOError:
+        print "Could not read data from file"
 
-	# Read Trace-Event
-	length = struct.unpack('!I', lengthNO)[0]
-	trace_event.ParseFromString(f.read(length))
+    # Read Trace-Event
+    length = struct.unpack('!I', lengthNO)[0]
+    trace_event.ParseFromString(f.read(length))
 
-	# This works for any type of pb message:
-	#print trace_event
+    # This works for any type of pb message:
+    #print trace_event
 
-	# More compact dump for traces:
-	if not trace_event.HasField("memaddr"):
-		print "IP {0:x}".format(trace_event.ip)
-	else:
-		print "MEM {0} {1:x} width {2:d} IP {3:x}".format(
-		"R" if trace_event.accesstype == trace_pb2.Trace_Event.READ else "W",
-		trace_event.memaddr, trace_event.width, trace_event.ip)
+    # More compact dump for traces:
+    if not trace_event.HasField("memaddr"):
+        print "IP {0:x}".format(trace_event.ip)
+    else:
+        print "MEM {0} {1:x} width {2:d} IP {3:x}".format(
+        "R" if trace_event.accesstype == trace_pb2.Trace_Event.READ else "W",
+        trace_event.memaddr, trace_event.width, trace_event.ip)
 
 f.close()
