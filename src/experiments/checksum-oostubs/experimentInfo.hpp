5259f3497b1e916b1ee9d182134a1663ba9f4887
chb <chb@8c4709b5-6ec9-48aa-a5cd-a96041d1645a>
2012-06-22 14:19:35 +0000

    checksum-oostubs experiment update for new ecc aspects
    git-svn-id: https://www4.informatik.uni-erlangen.de/i4svn/danceos/trunk/devel/fail@1384 8c4709b5-6ec9-48aa-a5cd-a96041d1645a

--- src/experiments/checksum-oostubs/experimentInfo.hpp
+++ src/experiments/checksum-oostubs/experimentInfo.hpp
@@ -10,27 +10,27 @@
 #define OOSTUBS_FUNC_ENTRY		0x00101eaa
 // empty function that is called explicitly when the experiment finished
 // nm -C ecc.elf|fgrep "finished()"
-#define OOSTUBS_FUNC_FINISH		0x00106a70
+#define OOSTUBS_FUNC_FINISH		0x00106da4
 // function executing HLT with no chance for further progress (after panic())
 // nm -C ecc.elf|fgrep cpu_halt
 #define OOSTUBS_FUNC_CPU_HALT	0x001009f7
 
 // nm -C ecc.elf | fgrep "_TEXT_"
 #define OOSTUBS_TEXT_START		0x00100000
-#define OOSTUBS_TEXT_END		0x0010797b
+#define OOSTUBS_TEXT_END		0x00107cbf
 
 // number of instructions the target executes under non-error conditions from ENTRY to DONE:
 // (result of experiment's step #2)
-#define OOSTUBS_NUMINSTR		0x4340ED
+#define OOSTUBS_NUMINSTR		0x4B16E6
 // number of instructions that are executed additionally for error corrections
 // (this is a rough guess ... TODO)
 #define OOSTUBS_RECOVERYINSTR	0x2000
 // the variable that's increased if ECC corrects an error:
 // nm -C ecc.elf|fgrep errors_corrected
-#define OOSTUBS_ERROR_CORRECTED	0x0010b7b0
+#define OOSTUBS_ERROR_CORRECTED	0x0010baf0
 //
 // nm -C ecc.elf|fgrep results
-#define OOSTUBS_RESULTS_ADDR	0x0010ab2c
+#define OOSTUBS_RESULTS_ADDR	0x0010ae6c
 #define OOSTUBS_RESULTS_BYTES	12
 #define OOSTUBS_RESULT0			0xab3566a9
 #define OOSTUBS_RESULT1			0x44889112
