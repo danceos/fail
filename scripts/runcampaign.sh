394e10ca6f3bb9b1d9d14110a6cd747c30f43de8
chb <chb@8c4709b5-6ec9-48aa-a5cd-a96041d1645a>
2012-08-28 09:26:52 +0000

    fail*: redirect output of server to file ... but where?
    git-svn-id: https://www4.informatik.uni-erlangen.de/i4svn/danceos/trunk/devel/fail@1576 8c4709b5-6ec9-48aa-a5cd-a96041d1645a

--- scripts/runcampaign.sh
+++ scripts/runcampaign.sh
@@ -22,7 +22,7 @@ if [ ! -x "$1" ]; then echo "usage: $0 <campaignserver-binary>" >&2; exit 1; fi
 CAMPAIGNSERVER_BINARY=$1
 
 date
-/usr/bin/time -po .time "$CAMPAIGNSERVER_BINARY" >/dev/null 2>&1 &
+/usr/bin/time -po .time "$CAMPAIGNSERVER_BINARY" > $(basename "$CAMPAIGNSERVER_BINARY")_progress.txt 2>&1 &
 sleep .1 # wait until server is likely to have a listening socket opened
 
 for h in $FAIL_EXPERIMENT_HOSTS
