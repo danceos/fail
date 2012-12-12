d019f64bf50a36e7a6348d298d38750f99e50cde
adrian <adrian@8c4709b5-6ec9-48aa-a5cd-a96041d1645a>
2012-12-12 13:08:35 +0000

    Export Bochs 64 bit ability (if enabled).
    This is required to add the architecture-dependent registers.
    
    git-svn-id: https://www4.informatik.uni-erlangen.de/i4svn/danceos/trunk/devel/fail@1979 8c4709b5-6ec9-48aa-a5cd-a96041d1645a

--- src/core/sal/bochs/BochsConfig.hpp
+++ src/core/sal/bochs/BochsConfig.hpp
@@ -20,6 +20,11 @@ typedef Bit8u*     host_address_t;  //!< the host memory address type
 #endif
 typedef int        timer_t;         //!< type of timer IDs
 
+// 'Publish' 64 bit ability (if enabled in Bochs):
+#if BX_SUPPORT_X86_64
+  #define SIM_SUPPORT_64
+#endif
+
 } // end-of-namespace: fail
 
 #endif // __BOCHS_CONFIG_HPP__
