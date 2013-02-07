7f587d461c2daab31c8d683e8039387bdbd48218
adrian <adrian@8c4709b5-6ec9-48aa-a5cd-a96041d1645a>
2013-02-07 14:17:49 +0000

    ArmArchitecture::fillRegisterList(): set textual register name appropriately
    For some reasons, the compiler cannot find a matching Register::setName(const std::string&) although it is implemented in sal/Register.cc. The work around fixes this issue.
    
    git-svn-id: https://www4.informatik.uni-erlangen.de/i4svn/danceos/trunk/devel/fail@2077 8c4709b5-6ec9-48aa-a5cd-a96041d1645a

--- src/core/sal/arm/Architecture.cc
+++ src/core/sal/arm/Architecture.cc
@@ -18,9 +18,7 @@ void ArmArchitecture::fillRegisterList()
 		// Build and set the register name:
 		std::stringstream sstr;
 		sstr << "R" << i+1;
-		// FIXME This doesn't work because no matching setName is found.
-		// Not sure why this happens.
-		//reg->setName(sstr.str());
+		reg->setName(sstr.str().c_str());
 		m_addRegister(reg);
 	}
 
