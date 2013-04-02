danceos/fail

commit eb17e9ef8260b2799f8edf7de86e2d8fa97da85c

core/sal: move command-line parameter passing to SC::startup()

src/core/sal/SimulatorController.cc
@@ -2,6 +2,7 @@
 #include "SALInst.hpp"
 #include "Event.hpp"
 #include "Listener.hpp"
+#include "util/CommandLine.hpp"
 
 namespace fail {
 
@@ -35,12 +36,16 @@ BaseListener* SimulatorController::resume(void)
 	return m_LstList.getLastFired();
 }
 
-void SimulatorController::startup()
+void SimulatorController::startup(int argc, char **argv)
 {
 	// Some greetings to the user:
 	std::cout << "[SimulatorController] Initializing..." << std::endl;
 	// TODO: Log-Level?
 	
+	if (argv) {
+		CommandLine::Inst().collect_args(bx_startup_flags.argc, bx_startup_flags.argv);
+	}
+
 	// Activate previously added experiments to allow initialization:
 	initExperiments();
 }

src/core/sal/SimulatorController.hpp
@@ -48,8 +48,11 @@ public:
 	 * This function needs to be invoked once the simulator starts, and
 	 * allows the SimulatorController to instantiate all needed experiment
 	 * components.
+	 *
+	 * @param argc main()'s argument counter
+	 * @param argv main()'s argument value vector
 	 */
-	void startup();
+	void startup(int argc = 0, char **argv = 0);
 	/**
 	 * Experiments need to hook here.
 	 */

src/core/sal/bochs/BochsController.cc
@@ -4,7 +4,6 @@
 #include "BochsMemory.hpp"
 #include "../SALInst.hpp"
 #include "../Listener.hpp"
-#include "util/CommandLine.hpp"
 
 namespace fail {
 
@@ -172,11 +171,6 @@ ConcreteCPU& BochsController::detectCPU(BX_CPU_C* pCPU) const
 	}
   #endif
 	return getCPU(i);
-}
-
-void BochsController::collectCommandLineArguments(int argc, char **argv) const
-{
-	CommandLine::Inst().collect_args(bx_startup_flags.argc, bx_startup_flags.argv);
 }
 
 } // end-of-namespace: fail

src/core/sal/bochs/BochsController.hpp
@@ -150,20 +150,6 @@ public:
 	 * @see The uses SimulatorController::getCPU().
 	 */
 	ConcreteCPU& detectCPU(BX_CPU_C* pCPU) const;
-
-protected:
-	/**
-	 * Hack: Indirection for commandline argument collection
-	 *
-	 * This prevents CommandLine.hpp (and optionparser.h) from being pulled
-	 * inbe to every single Bochs translation unit via FailBochsInit.ah,
-	 * leading to compilation errors in some of them.
-	 * TODO: Move this upwards to SimulatorController?
-	 *
-	 * @param argc main()'s argument counter
-	 * @param argv main()'s argument value vector
-	 */
-	void collectCommandLineArguments(int argc, char **argv) const;
 };
 
 } // end-of-namespace: fail

src/core/sal/bochs/FailBochsInit.ah
@@ -10,8 +10,7 @@
 aspect FailBochsInit {
 	advice call("int bxmain()") : before ()
 	{
-        fail::simulator.collectCommandLineArguments(bx_startup_flags.argc, bx_startup_flags.argv);
-		fail::simulator.startup();
+		fail::simulator.startup(bx_startup_flags.argc, bx_startup_flags.argv);
 	}
 };
 

src/core/sal/gem5/Gem5Controller.cc
@@ -19,6 +19,7 @@ void Gem5Controller::startup()
 		addCPU(cpu);
 	}
 
+	// TODO pass on command-line parameters
 	SimulatorController::startup();
 }
 

src/core/sal/ovp/FailOVPInit.ah
@@ -14,6 +14,7 @@ aspect FailOVPInit {
 	{
 		std::cout << "OVP init aspect!" << std::endl;
 		// TODO: Log-Level?
+		// TODO pass on command-line parameters
 		fail::simulator.startup();
 	}
 };

src/core/sal/qemu/wrappers.cc
@@ -14,6 +14,7 @@ void fail_init(struct CPUX86State *env)
 {
 	std::cout << "FailQEMU v" FAIL_VERSION << std::endl;
 	fail::simulator.setCPUEnv(env);
+	// TODO pass on command-line parameters
 	fail::simulator.startup();
 }
 

commit 3fb58de4a55942ae4f51767875cdf0bdcd682a9e

core/sal: pass commandline parameters by reference
This allows the commandline parameter parser to modify argc, as it finds
arguments for the Fail* client.  Additionally argv is correctly null
terminated when removing arguments.

This fixes a bug introduced in eb17e9ef8260b2799f8edf7de86e2d8fa97da85c.

Change-Id: Iabe84530790ecb7c587b0af139127015aad868d5

src/core/sal/SimulatorController.cc
@@ -36,15 +36,19 @@ BaseListener* SimulatorController::resume(void)
 	return m_LstList.getLastFired();
 }
 
-void SimulatorController::startup(int argc, char **argv)
+void SimulatorController::startup(int& argc, char **& argv)
+{
+	if (argv) {
+		CommandLine::Inst().collect_args(argc, argv);
+	}
+	startup();
+}
+
+void SimulatorController::startup()
 {
 	// Some greetings to the user:
 	std::cout << "[SimulatorController] Initializing..." << std::endl;
 	// TODO: Log-Level?
-
-	if (argv) {
-		CommandLine::Inst().collect_args(argc, argv);
-	}
 
 	// Activate previously added experiments to allow initialization:
 	initExperiments();

src/core/sal/SimulatorController.hpp
@@ -52,7 +52,11 @@ public:
 	 * @param argc main()'s argument counter
 	 * @param argv main()'s argument value vector
 	 */
-	void startup(int argc = 0, char **argv = 0);
+	void startup(int& argc, char **& argv);
+	/**
+	 * @brief Parameter-less version of startup() for backends that do not (yet) handle parameters
+	 */
+	void startup();
 	/**
 	 * Experiments need to hook here.
 	 */

src/core/util/CommandLine.cc
@@ -13,7 +13,8 @@ namespace fail {
             if (strncmp(argv[i], "-Wf,", 4) == 0) {
                 this->argv.push_back(std::string(argv[i] + 4));
 
-                for (int x = i + 1; x < argc; ++x) {
+                // also copy argv[argc], which equals 0
+                for (int x = i + 1; x <= argc; ++x) {
                     argv[x - 1] = argv[x];
                 }
                 i --;