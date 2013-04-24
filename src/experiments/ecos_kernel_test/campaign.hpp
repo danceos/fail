0887a53d1df9297180854767af3035987db1af7b
Horst Schirmeier <horst.schirmeier@tu-dortmund.de>
2013-04-24 17:19:38 +0200

    ecos: campaign rewrite for MySQL data store
    We're not yet using the common DatabaseCampaign as it doesn't allow
    for additional experiment parameters (such as "variant" and
    "benchmark") yet.  TODO: Integrate changes in DatabaseCampaign in a
    generic way and use it.
    
    Change-Id: I45480003be433654aea8d3a417fbfa66be31155b

--- src/experiments/ecos_kernel_test/campaign.hpp
+++ src/experiments/ecos_kernel_test/campaign.hpp
@@ -2,11 +2,13 @@
 
 #include <string>
 #include <fstream>
+#include <mysql/mysql.h>
 
 #ifndef __puma
 #include <boost/thread.hpp>
 #endif
 
+#include "util/Database.hpp"
 #include "cpn/Campaign.hpp"
 #include "comm/ExperimentData.hpp"
 #include "ecos_kernel_test.pb.h"
@@ -20,33 +22,21 @@ public:
 };
 
 class EcosKernelTestCampaign : public fail::Campaign {
+	fail::Database *db;
+	fail::Database *db_recv;
+	int fspmethod_id;
+
 	static const std::string dir_images;
 	static const std::string dir_prerequisites;
-	static const std::string dir_results;
+	std::string m_result_table;
 	fail::Logger m_log;
-	int count_exp, count_exp_jobs;
-	int count_known, count_known_jobs;
-	bool add_experiment_ec(const std::string& variant, const std::string& benchmark,
-		fail::address_t data_address, int instr1, int instr2, fail::address_t instr_absolute);
-	bool add_known_ec(const std::string& variant, const std::string& benchmark,
-		fail::address_t data_address, int instr1, int instr2, fail::address_t instr_absolute);
-	bool init_results();
-	void add_result(const std::string& variant, const std::string& benchmark,
-		int instr1, int instr2, fail::address_t instr2_absolute, fail::address_t ec_data_address,
+	void add_result(unsigned pilot_id,
+		int instr2, fail::address_t instr2_absolute, fail::address_t ec_data_address,
 		int bitnr, int bit_width, int resulttype, int ecos_test_result, fail::address_t latest_ip,
 		int error_corrected, const std::string& details, float runtime);
-	void finalize_results();
 	void collect_results();
-	bool check_available(const std::string& variant, const std::string& benchmark, fail::address_t data_address, int instr2);
-	std::ofstream resultstream;
-	typedef std::map<std::pair<const std::string, const std::string>, std::map<fail::address_t, std::set<int> > > AvailableResultMap;
-	AvailableResultMap available_results;
-#ifndef __puma
-	boost::mutex m_result_mutex;
-#endif
 public:
-	EcosKernelTestCampaign() : m_log("EcosKernelTest Campaign"),
-		count_exp(0), count_exp_jobs(0), count_known(0), count_known_jobs(0) {}
+	EcosKernelTestCampaign() : m_log("EcosKernelTest Campaign") {}
 	virtual bool run();
 	static bool readMemoryMap(fail::MemoryMap &mm, char const * const filename);
 	static bool writeTraceInfo(unsigned instr_counter, unsigned timeout, unsigned mem1_low, unsigned mem1_high, unsigned mem2_low, unsigned mem2_high, const std::string& variant = "", const std::string& benchmark = "");
@@ -55,6 +45,5 @@ public:
 	static std::string filename_state(unsigned instr_offset, const std::string& variant = "", const std::string& benchmark = "");
 	static std::string filename_trace(const std::string& variant = "", const std::string& benchmark = "");
 	static std::string filename_traceinfo(const std::string& variant = "", const std::string& benchmark = "");
-	static std::string filename_results(const std::string& variant = "", const std::string& benchmark = "");
 	static std::string filename_elf(const std::string& variant = "", const std::string& benchmark = "");
 };
