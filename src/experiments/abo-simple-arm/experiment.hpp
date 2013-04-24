628c1b6997c6e9c1ecfdb21cc1d0b6c6a52cef83
Adrian Böckenkamp <adrian.boeckenkamp@tu-dortmund.de>
2013-04-24 14:26:11 +0200

    Added abo-simple-arm experiment code
    It is a simple and short experiment that performs single stepping
    through the target. It is designed to be used with gem5 (+ ARM).
    
    Change-Id: Id48b2b087a3650bd0298454ff168c0dbdaaae0c8

---
+++ src/experiments/abo-simple-arm/experiment.hpp
@@ -0,0 +1,13 @@
+#ifndef __ABO_SIMPLE_ARM_EXPERIMENT_HPP__
+  #define __ABO_SIMPLE_ARM_EXPERIMENT_HPP__
+  
+#include "efw/ExperimentFlow.hpp"
+#include "efw/JobClient.hpp"
+
+class ABOSimpleARMExperiment : public fail::ExperimentFlow {
+public:
+	ABOSimpleARMExperiment() { }
+	bool run();
+};
+
+#endif // __ABO_SIMPLE_ARM_EXPERIMENT_HPP__
