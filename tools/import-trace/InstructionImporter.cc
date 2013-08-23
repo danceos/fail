danceos/fail

commit 52322ac7b97f49a3b5726d54d3762d9f6857b07c

import-trace: compile with LLVM 3.3
This was necessary because LLVM 3.1's
ELFObjectFile<ELFT>::sectionContainsSymbolsectionContainsSymbol() (needed
for proper functioning of our LLVM disassembler, unless you're using
-ffunction-sections) is "unimplemented".

Change-Id: I81112627ebd1c92b718ac6f4ed58d7f188aedf0c

tools/import-trace/InstructionImporter.cc
@@ -24,7 +24,7 @@ bool InstructionImporter::handle_ip_event(fail::simtime_t curtime, instruction_c
 			return false;
 		}
 
-		ObjectFile *obj = dyn_cast<ObjectFile>(binary.get());
+		ObjectFile *obj = dyn_cast<ObjectFile, Binary>(binary.get());
 
 		disas.reset(new LLVMDisassembler(obj));
 		disas->disassemble();

tools/import-trace/RandomJumpImporter.cc
@@ -70,7 +70,7 @@ bool RandomJumpImporter::handle_ip_event(fail::simtime_t curtime, instruction_co
 			return false;
 		}
 
-		ObjectFile *obj = dyn_cast<ObjectFile>(binary.get());
+		ObjectFile *obj = dyn_cast<ObjectFile, Binary>(binary.get());
 
 		disas.reset(new LLVMDisassembler(obj));
 		disas->disassemble();

tools/import-trace/RegisterImporter.cc
@@ -108,7 +108,7 @@ bool RegisterImporter::handle_ip_event(fail::simtime_t curtime, instruction_coun
 			return false;
 		}
 
-		ObjectFile *obj = dyn_cast<ObjectFile>(binary.get());
+		ObjectFile *obj = dyn_cast<ObjectFile, Binary>(binary.get());
 
 		disas.reset(new LLVMDisassembler(obj));
 		disas->disassemble();

commit 64840c02e4882a586ca15e6be03acc7c0153257a

import-trace: compile with both LLVM 3.1+3.3
Commit 52322ac broke compilation with LLVM 3.1.

Change-Id: Ia0e1d91258e6be799cfbd3b6e9693894a6bc5650

tools/import-trace/InstructionImporter.cc
@@ -24,9 +24,11 @@ bool InstructionImporter::handle_ip_event(fail::simtime_t curtime, instruction_c
 			return false;
 		}
 
-		ObjectFile *obj = dyn_cast<ObjectFile, Binary>(binary.get());
-
+// necessary due to an AspectC++ bug triggered by LLVM 3.3's dyn_cast()
+#ifndef __puma
+		ObjectFile *obj = llvm::dyn_cast<ObjectFile>(binary.get());
 		disas.reset(new LLVMDisassembler(obj));
+#endif
 		disas->disassemble();
 		LLVMDisassembler::InstrMap &instr_map = disas->getInstrMap();
 		LOG << "instructions disassembled: " << instr_map.size() << " Triple: " << disas->GetTriple() <<  std::endl;

tools/import-trace/RandomJumpImporter.cc
@@ -70,9 +70,11 @@ bool RandomJumpImporter::handle_ip_event(fail::simtime_t curtime, instruction_co
 			return false;
 		}
 
-		ObjectFile *obj = dyn_cast<ObjectFile, Binary>(binary.get());
-
+// necessary due to an AspectC++ bug triggered by LLVM 3.3's dyn_cast()
+#ifndef __puma
+		ObjectFile *obj = dyn_cast<ObjectFile>(binary.get());
 		disas.reset(new LLVMDisassembler(obj));
+#endif
 		disas->disassemble();
 		LLVMDisassembler::InstrMap &instr_map = disas->getInstrMap();
 		LOG << "instructions disassembled: " << instr_map.size() << " Triple: " << disas->GetTriple() <<  std::endl;

tools/import-trace/RegisterImporter.cc
@@ -108,9 +108,11 @@ bool RegisterImporter::handle_ip_event(fail::simtime_t curtime, instruction_coun
 			return false;
 		}
 
-		ObjectFile *obj = dyn_cast<ObjectFile, Binary>(binary.get());
-
+// necessary due to an AspectC++ bug triggered by LLVM 3.3's dyn_cast()
+#ifndef __puma
+		ObjectFile *obj = dyn_cast<ObjectFile>(binary.get());
 		disas.reset(new LLVMDisassembler(obj));
+#endif
 		disas->disassemble();
 		LLVMDisassembler::InstrMap &instr_map = disas->getInstrMap();
 		LOG << "instructions disassembled: " << instr_map.size() << " Triple: " << disas->GetTriple() <<  std::endl;