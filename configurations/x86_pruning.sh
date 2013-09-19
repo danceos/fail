c6e298ac6fa78fd4465ee3b0014bc73506d10e58
Martin Hoffmann <hoffmann@cs.fau.de>
2013-09-19 13:05:44 +0200

    Helper script for FailBochs pruning build env
    Call this script from a build directory to setup
    a build environment for FailBochs pruning.
    
    Usage:
     <path to fail>/configurations/x86_pruning.sh <Experiment name>
    
    Change-Id: I778a20c258dcd5349c3da9e1c5a8542af43f61d5

---
+++ configurations/x86_pruning.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+## A cmake configuration call for a FailBochs pruning
+EXP=$1
+FAILPATH=$(dirname $0)/..
+if [ -z $1 ]
+then
+  echo "Experiment not set. Usage: $0 <experiment name>"
+  echo "Existing experiments:"
+  find ${FAILPATH}/src/experiments -maxdepth 1 -mindepth 1 -type d -printf \-\>\ %P\\n | sort
+else
+
+cmake ${FAILPATH} -DBUILD_BOCHS:BOOL=ON -DBUILD_X86:BOOL=ON  -DCONFIG_BOCHS_NO_ABORT:BOOL=ON -DCONFIG_EVENT_BREAKPOINTS:BOOL=ON -DCONFIG_EVENT_BREAKPOINTS_RANGE:BOOL=ON -DCONFIG_EVENT_INTERRUPT:BOOL=ON -DCONFIG_EVENT_IOPORT:BOOL=ON -DCONFIG_EVENT_MEMREAD:BOOL=ON -DCONFIG_EVENT_MEMWRITE:BOOL=ON -DCONFIG_EVENT_TRAP:BOOL=ON -DCONFIG_SR_RESTORE:BOOL=ON -DCONFIG_SR_SAVE:BOOL=ON -D"bochs_configure_params:STRING=--enable-a20-pin;--enable-x86-64;--enable-cpu-level=6;--enable-ne2000;--enable-acpi;--enable-pci;--enable-usb;--enable-trace-cache;--enable-fast-function-calls;--enable-host-specific-asms;--enable-disasm;--enable-readline;--enable-clgd54xx;--enable-fpu;--enable-vmx=2;--enable-monitor-mwait;--enable-cdrom;--enable-sb16=linux;--enable-gdb-stub;--with-nogui" -DEXPERIMENTS_ACTIVATED:STRING=${EXP} -DBUILD_IMPORT_TRACE:BOOL=ON -DBUILD_PRUNE_TRACE:BOOL=ON -DBUILD_LLVM_DISASSEMBLER=ON
+fi
