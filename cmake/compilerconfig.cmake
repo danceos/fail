bae03bc929c33a96bb9b5622104f960e32b81a01
Horst Schirmeier <horst.schirmeier@tu-dortmund.de>
2014-04-27 18:59:23 +0200

    cmake: build with -D__NO_MATH_INLINES
    To use the new clang-based AspectC++ (ac++ >1.2), Fail* must be built
    with -D__NO_MATH_INLINES (or --c_compiler clang++).
    
    Change-Id: I020d62891946d09ac456b8df914b669ab33ab0e2

--- cmake/compilerconfig.cmake
+++ cmake/compilerconfig.cmake
@@ -12,7 +12,7 @@ set(CMAKE_EXE_LINKER_FLAGS "-Wl,-gc-sections")
 
 set(CMAKE_C_COMPILER "gcc")
 set(CMAKE_CXX_COMPILER "ag++")
-set(CMAKE_AGPP_FLAGS "--real-instances" CACHE STRING "Additional ag++ flags, e.g. --real-instances --keep_woven")
+set(CMAKE_AGPP_FLAGS "-D__NO_MATH_INLINES" CACHE STRING "Additional ag++ flags (space-separated), e.g., --keep_woven")
   ## Here we add the build dir holding the generated header files (protobuf)
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --Xweaver -p ${PROJECT_SOURCE_DIR}/src -p ${PROJECT_SOURCE_DIR}/simulators -p ${PROJECT_SOURCE_DIR}/debuggers -p ${PROJECT_SOURCE_DIR}/tools -p ${PROJECT_BINARY_DIR}/src ${CMAKE_AGPP_FLAGS} --Xcompiler")
 
