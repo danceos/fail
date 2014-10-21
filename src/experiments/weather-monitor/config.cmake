0317be8f8f84a28430897e00dbc63907ae111e32
Christian Dietrich <christian.dietrich@informatik.uni-erlangen.de>
2014-10-21 17:57:44 +0200

    cmake: Replace the experiment configuration mechanism
    The inline configuration mechanism did work properly for iterated
    invocation of cmake. But since the configuration items were set after
    the decision was made whether a tool was build or not, they had only
    influence on the second run of cmake.
    
    The experiment configuration is no saved in a config.cmake file, which
    populates the CMakeCache.txt before any other CMakeLists.txt is read.
    
    Change-Id: I6eca1c6e462af3a241bd9c5b9a27a71a1f5d2829

---
+++ src/experiments/weather-monitor/config.cmake
@@ -0,0 +1,4 @@
+## Configuration
+SET(PLUGINS_ACTIVATED "tracing" CACHE STRING "")
+
+SET(bochs_configure_params "--enable-a20-pin;--enable-x86-64;--enable-cpu-level=6;--enable-ne2000;--enable-acpi;--enable-pci;--enable-usb;--enable-trace-cache;--enable-fast-function-calls;--enable-host-specific-asms;--enable-disasm;--enable-readline;--enable-clgd54xx;--enable-fpu;--enable-vmx=2;--enable-monitor-mwait;--enable-cdrom;--enable-sb16=linux;--enable-gdb-stub;--with-nogui" CACHE STRING "")
