b8c03bca8d91ac9c457a251ffed16e23d902904b
Horst Schirmeier <horst.schirmeier@tu-dortmund.de>
2015-01-28 18:43:31 +0100

    fail-env: assign -> use default values
    In bash, ${parameter:=word} assigns the default value.  In our case,
    ${parameter:-word} (the whole expression has a default value, but it's
    not assigned) suffices.
    
    Change-Id: I5f4105b973892aa13943d660cbebaed76fd43752

--- scripts/fail-env.sh
+++ scripts/fail-env.sh
@@ -9,16 +9,16 @@
 # A whitespace-separated list of hosts to rsync the experiment data to.  This
 # is not necessarily the same list as FAIL_EXPERIMENT_HOSTS (see below), as
 # many hosts may share their homes via NFS.
-export FAIL_DISTRIBUTE_HOSTS=${FAIL_DISTRIBUTE_HOSTS:="ios kos virtuos plutonium bigbox.informatik.uni-erlangen.de ls12sp $(for hostnr in $(seq 39 58); do echo -n cloudhost$hostnr\ ; done)"}
+export FAIL_DISTRIBUTE_HOSTS=${FAIL_DISTRIBUTE_HOSTS:-"ios kos virtuos plutonium bigbox.informatik.uni-erlangen.de ls12sp $(for hostnr in $(seq 39 58); do echo -n cloudhost$hostnr\ ; done)"}
 
 # A whitespace-separated list of hosts to run experiments on.  If the host name
 # is followed by a ':' and a number, this specifies the number of clients to
 # run on that host (defaults to #CPUs).
-export FAIL_EXPERIMENT_HOSTS=${FAIL_EXPERIMENT_HOSTS:="bigbox.informatik.uni-erlangen.de plutonium:4 uran:4 virtuos ios:32 kos:16 bohrium:12 polonium:12 radon $(for hostnr in $(seq 100 254); do echo -n fiws$hostnr\ ; done) $(for hostnr in $(seq 39 58); do echo -n cloudhost$hostnr\ ; done)"}
+export FAIL_EXPERIMENT_HOSTS=${FAIL_EXPERIMENT_HOSTS:-"bigbox.informatik.uni-erlangen.de plutonium:4 uran:4 virtuos ios:32 kos:16 bohrium:12 polonium:12 radon $(for hostnr in $(seq 100 254); do echo -n fiws$hostnr\ ; done) $(for hostnr in $(seq 39 58); do echo -n cloudhost$hostnr\ ; done)"}
 
 # A homedir-relative directory on the distribution hosts where all necessary
 # Fail* ingredients reside (see multiple-clients.sh).
-export FAIL_EXPERIMENT_TARGETDIR=${FAIL_EXPERIMENT_TARGETDIR:=.fail-experiment}
+export FAIL_EXPERIMENT_TARGETDIR=${FAIL_EXPERIMENT_TARGETDIR:-.fail-experiment}
 
 # Number of parallel build processes.  If unset, #CPUs+1.
 if [ -z "$FAIL_BUILD_PARALLEL" ]; then
